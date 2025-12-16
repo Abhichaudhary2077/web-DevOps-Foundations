f07d4b59392f51ab00d2e7a88046c5fc73e6cd2e
Abhi choudhary <154852841+Abhichaudhary2077@users.noreply.github.com>
2025-12-17 01:06:01 +0530

    Javascript edited for CI

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const themeBtn = document.getElementById("themeBtn");
+if (typeof document !== "undefined") {
+  const themeBtn = document.getElementById("themeBtn");
 
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    themeBtn.textContent = "Light Theme";
-  } else {
-    themeBtn.textContent = "Dark Theme";
-  }
-});
+    if (document.body.classList.contains("dark")) {
+      themeBtn.textContent = "Light Theme";
+    } else {
+      themeBtn.textContent = "Dark Theme";
+    }
+  });
+}
