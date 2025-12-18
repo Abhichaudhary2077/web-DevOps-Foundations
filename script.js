a5a4dc399df23bb9528d28b483ea1e1ff5193cf2
Abhi choudhary <154852841+Abhichaudhary2077@users.noreply.github.com>
2025-12-18 13:22:38 +0530

    Added More  portfolio Enchancement

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const themeBtn = document.getElementById("themeBtn");
 
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-
-  const isDark = document.body.classList.contains("dark");
-  themeBtn.textContent = isDark ? "Light Mode" : "Dark Mode";
+  themeBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
