3c52b1a5508c498e51b64bdbf22a61e2b517b9e1
Abhi choudhary <154852841+Abhichaudhary2077@users.noreply.github.com>
2025-12-18 11:34:42 +0530

    Optimised Portfolio

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-if (typeof document !== "undefined") {
-  const themeBtn = document.getElementById("themeBtn");
+const themeBtn = document.getElementById("themeBtn");
 
-  themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 
-    if (document.body.classList.contains("dark")) {
-      themeBtn.textContent = "Light Theme";
-    } else {
-      themeBtn.textContent = "Dark Theme";
-    }
-  });
-}
+  const isDark = document.body.classList.contains("dark");
+  themeBtn.textContent = isDark ? "Light Mode" : "Dark Mode";
+});
